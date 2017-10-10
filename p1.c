56035f868e7ba6275c091eadb56865e3851cc369
jannelouisea <janne.louise.a@gmail.com>
2017-10-10 15:47:47 -0400

    Fixed p1, started p2

--- p1.c
+++ p1.c
@@ -8,12 +8,15 @@ pthread_t tid[3];
 
 void * loop_print(void * arg)
 {
+  // printf("Starting new thread\n");
   int *args = arg;
   unsigned long i = 0;
   int j = args[0];
+  // printf("j: %d\n", j);
   int k = args[1];
+  // printf("k: %d\n", k);
 
-  int cnt[2];
+  int cnt[2+k];
 
   while(j > 0)
   {
@@ -36,11 +39,12 @@ int main()
   int err;
 
   int * args;
-  while(i < 3)
-  {
-    args = (int *)malloc(sizeof(int)*2);
-    args[0] = j;
-    args[1] = k;
+  while(i < 3) {
+      args = (int *)malloc(sizeof(int)*2);
+      args[0] = j;
+      args[1] = k;
+      //printf("im ohk here\n");
+
     err = pthread_create(&(tid[i]), NULL, &loop_print, args);
     if (err != 0)
     {
--- p2.c
+++ p2.c
@@ -4,8 +4,16 @@
 #include <pthread.h>
 
 #define STR_ARR_LEN 3
+#define NUM_OF_THREADS 3
+#define THREAD0 0
+#define THREAD1 2
+#define THREAD2 1
 
 //////// add initialization code for condition variable here ////////
+pthread_mutex_t lock    = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond     = PTHREAD_COND_INITIALIZER;
+unsigned total          = 0;
+
 
 // Print an error message and exit on an error
 void fail(char *msg) {
@@ -16,22 +24,35 @@ void fail(char *msg) {
 // Global variable, to tell all the threads when it's time to stop.
 volatile int running = 1;
 
+void updateRunning() {
+    total++;
+    if (total >= 9) {
+        running = 0;
+    }
+}
+
 // Start routine for thread0
 void *thread0(void *arg) {
     char ** str_pt = (char **)arg;
     unsigned i=0;
     while (running) {
 
+        // add code to wait
+        pthread_mutex_lock(&lock);
+        while ((total % NUM_OF_THREADS) != THREAD0) {
+            pthread_cond_wait(&cond, &lock);
+        }
+        pthread_mutex_unlock(&lock);
+
         //////// add condition variable synchronization here ////////
-
-
+        pthread_mutex_lock(&lock);
         // print string in sequence.
         printf("%s\n", str_pt[i%STR_ARR_LEN]);
-
-        //////// add condition variable synchronization here ////////
-
-
         i++;
+        updateRunning();
+        pthread_cond_signal(&cond);
+        pthread_mutex_unlock(&lock);
+        //////// add condition variable synchronization here ////////
     }
     printf("Thread 0 total count %u\n",i);
     return NULL;
@@ -43,14 +64,22 @@ void *thread1(void *arg) {
     unsigned i=0;
     while (running) {
 
-        //////// add condition variable synchronization here ////////
+        // add code to wait
+        pthread_mutex_lock(&lock);
+        while ((total % NUM_OF_THREADS) != THREAD1) {
+            pthread_cond_wait(&cond, &lock);
+        }
+        pthread_mutex_unlock(&lock);
 
+        //////// add condition variable synchronization here ////////
+        pthread_mutex_lock(&lock);
         // print string in sequence.
         printf("%s\n", str_pt[i%STR_ARR_LEN]);
-
-        //////// add condition variable synchronization here ////////
-
         i++;
+        updateRunning();
+        pthread_cond_signal(&cond);
+        pthread_mutex_unlock(&lock);
+        //////// add condition variable synchronization here ////////
     }
     printf("Thread 1 total count %u\n",i);
 
@@ -63,14 +92,22 @@ void *thread2(void *arg) {
     unsigned i=0;
     while (running) {
 
-        //////// add condition variable synchronization here ////////
+        // add code to wait
+        pthread_mutex_lock(&lock);
+        while ((total % NUM_OF_THREADS) != THREAD2) {
+            pthread_cond_wait(&cond, &lock);
+        }
+        pthread_mutex_unlock(&lock);
 
+        //////// add condition variable synchronization here ////////
+        pthread_mutex_lock(&lock);
         // print string in sequence.
         printf("%s\n", str_pt[i%STR_ARR_LEN]);
-
-        //////// add condition variable synchronization here ////////
-
         i++;
+        updateRunning();
+        pthread_cond_signal(&cond);
+        pthread_mutex_unlock(&lock);
+        //////// add condition variable synchronization here ////////
     }
     printf("Thread 2 total count %u\n",i);
 
